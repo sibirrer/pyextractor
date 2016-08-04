1a06cfd972edb84fcaa5160b5616346723102795
Simon Birrer <simon.birrer@pyhs.ethz.ch>
2016-08-04 09:05:58 +0200

    requirements for installatin added

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ The full documentation can be generated with Sphinx"""
 
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
-requires = [] #during runtime
+requires = ["numpy"] #during runtime
 tests_require=['pytest>=2.3'] #for testing
 
 PACKAGE_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
